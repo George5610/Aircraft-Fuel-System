486528f512ca7322b81f2bae2553996cad08f88d
George Johnson <georgejohnson5610@gmail.com>
2021-04-12 11:19:51 +0100

    Add files via upload

---
+++ TestPilot.m
@@ -0,0 +1,21 @@
+ 
+% TestPilot function argument u1
+% TestPilot function returns y1..y3
+
+function y = TestPilot(u)
+
+    % Unpack the inputs
+    time = u(1);
+    RefuelSwitch = u(2);
+    RefuelQuantity = u(3);
+    RightEngine = u(4);
+    LeftEngine = u(5);
+
+    
+    % Pack the outputs
+    y(1) = RefuelSwitch;
+    y(2) = RefuelQuantity;
+    y(3) = LeftEngine;
+    y(4) = RightEngine;
+       
+end
